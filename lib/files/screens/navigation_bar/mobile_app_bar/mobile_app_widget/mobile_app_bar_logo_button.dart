91f9a2a4ad347cf276aa26dc1a6e9117decb4a33
Shiv <sky.shivy@gmail.com>
2023-08-18 17:32:55 +0530

    bug fixed

--- lib/files/screens/navigation_bar/mobile_app_bar/mobile_app_widget/mobile_app_bar_logo_button.dart
+++ lib/files/screens/navigation_bar/mobile_app_bar/mobile_app_widget/mobile_app_bar_logo_button.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/instance_manager.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_buttons/custom_button.dart';
 import 'package:mtn_sa_revamp/files/utility/image_name.dart';
 
 Widget mobileAppBarLogoButton() {
   return Center(
     child: CustomButton(
+      onTap: () {
+        Get.until((route) => Get.currentRoute == '/');
+      },
       leftWidget: Image.asset(
         logoBigImg,
         height: 40,
--- lib/files/utility/route_direction.dart
+++ lib/files/utility/route_direction.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:mtn_sa_revamp/files/controllers/player_controller.dart';
 import 'package:mtn_sa_revamp/files/screens/my_tune_setting_screen/my_tune_settng_screen.dart';
+import 'package:mtn_sa_revamp/files/screens/navigation_bar/mobile_app_bar/mobile_app_bar.dart';
 import 'package:mtn_sa_revamp/files/utility/colors.dart';
 import 'package:mtn_sa_revamp/files/utility/route_name.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_text/custom_text.dart';
@@ -11,10 +12,19 @@ import 'package:mtn_sa_revamp/files/screens/profile_screen/profile_screen.dart';
 import 'package:mtn_sa_revamp/files/screens/wishlist_screen/wishlsit_screen.dart';
 import 'package:mtn_sa_revamp/files/screens/search_screen/artist_tune_screen.dart';
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_page_banner/sub_views/home_banner_detail_page.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 List<GetPage<dynamic>> get routesDirection {
   Duration dur = const Duration(milliseconds: 1);
   PlayerController controller = Get.find();
+  Widget reteurnWidget(Widget widget) {
+    return ResponsiveBuilder(
+      builder: (context, si) {
+        return si.isMobile ? MobileAppBar(widget: widget) : widget;
+      },
+    );
+  }
+
   return [
     GetPage(
         transitionDuration: dur,
@@ -29,9 +39,9 @@ List<GetPage<dynamic>> get routesDirection {
         String artist = Get.parameters['artist'] ?? '';
         controller.stop();
         return Material(
-          child: ArtistTuneScreen(
+          child: reteurnWidget(ArtistTuneScreen(
             artistName: artist,
-          ),
+          )),
         );
       },
     ),
@@ -40,7 +50,7 @@ List<GetPage<dynamic>> get routesDirection {
       name: profileTapped,
       page: () {
         controller.stop();
-        return Material(child: ProfileScreen());
+        return Material(child: reteurnWidget(ProfileScreen()));
       },
     ),
     GetPage(
@@ -48,7 +58,7 @@ List<GetPage<dynamic>> get routesDirection {
       name: wishlistTapped,
       page: () {
         controller.stop();
-        return Material(child: WishlistScreen());
+        return Material(child: reteurnWidget(WishlistScreen()));
       },
     ),
     GetPage(
@@ -68,12 +78,12 @@ List<GetPage<dynamic>> get routesDirection {
         String toneArtist = Get.parameters['toneArtist'] ?? '';
         String toneImage = Get.parameters['toneImage'] ?? '';
         return Material(
-            child: MyTuneSettingScreen(
+            child: reteurnWidget(MyTuneSettingScreen(
           toneId: toneId,
           toneName: toneName,
           toneArtist: toneArtist,
           toneImage: toneImage,
-        ));
+        )));
       },
     ),
     GetPage(
@@ -84,7 +94,8 @@ List<GetPage<dynamic>> get routesDirection {
         String categoryName = Get.parameters['categoryName'] ?? '';
         String categoryId = Get.parameters['categoryId'] ?? '';
         return Material(
-            child: CategoryScreen(category: categoryName, id: categoryId));
+            child: reteurnWidget(
+                CategoryScreen(category: categoryName, id: categoryId)));
       },
     ),
     GetPage(
@@ -96,8 +107,8 @@ List<GetPage<dynamic>> get routesDirection {
         String bannerOrder = Get.parameters['bannerOrder'] ?? '';
         String type = Get.parameters['type'] ?? '';
         return Material(
-            child: HomeBannerDetailPage(
-                type: type, bannerOrder: bannerOrder, searchKey: searchKey));
+            child: reteurnWidget(HomeBannerDetailPage(
+                type: type, bannerOrder: bannerOrder, searchKey: searchKey)));
       },
     ),
     GetPage(
