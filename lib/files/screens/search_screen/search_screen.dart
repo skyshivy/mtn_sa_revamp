2a984d203a5dc708841c66570d85403ac0da0a56
Shiv <sky.shivy@gmail.com>
2023-10-11 12:02:46 +0530

    new change

--- lib/files/screens/search_screen/search_screen.dart
+++ lib/files/screens/search_screen/search_screen.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mtn_sa_revamp/enums/font_enum.dart';
 import 'package:mtn_sa_revamp/files/controllers/search_controller/search_tune_controller.dart';
-import 'package:mtn_sa_revamp/files/custom_files/custom_grid_view/custom_grid_view.dart';
+
 import 'package:mtn_sa_revamp/files/custom_files/custom_load_more_data.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_text/custom_text.dart';
 import 'package:mtn_sa_revamp/files/custom_files/grid_delegate.dart';
