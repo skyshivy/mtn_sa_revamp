80ade0de43655af2db926a969e1dab19154f5376
Shiv <sky.shivy@gmail.com>
2023-11-14 15:23:09 +0530

    wishlis header added

--- lib/files/screens/wishlist_screen/wishlsit_screen.dart
+++ lib/files/screens/wishlist_screen/wishlsit_screen.dart
@@ -4,9 +4,12 @@ import 'package:get/instance_manager.dart';
 import 'package:mtn_sa_revamp/enums/font_enum.dart';
 import 'package:mtn_sa_revamp/files/controllers/wishlist_controller.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_text/custom_text.dart';
+import 'package:mtn_sa_revamp/files/custom_files/custom_top_header_view.dart';
 import 'package:mtn_sa_revamp/files/custom_files/grid_delegate.dart';
 import 'package:mtn_sa_revamp/files/custom_files/loading_indicator.dart';
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_recomended/sub_views/tune_cell.dart';
+import 'package:mtn_sa_revamp/files/service_call/header.dart';
+import 'package:mtn_sa_revamp/files/utility/string.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class WishlistScreen extends StatefulWidget {
@@ -38,11 +41,29 @@ class _WishlistScreenState extends State<WishlistScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      return wishlistController.isLoading.value
-          ? Center(child: loadingIndicator())
-          : gridView();
-    });
+    return ResponsiveBuilder(
+      builder: (context, si) {
+        return Obx(
+          () {
+            return wishlistController.isLoading.value
+                ? Center(child: loadingIndicator())
+                : Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const CustomTopHeaderView(title: wishlistStr),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: si.isMobile ? 8 : 30),
+                          child: gridView(),
+                        ),
+                      ),
+                    ],
+                  );
+          },
+        );
+      },
+    );
   }
 
   Widget gridView() {
@@ -51,6 +72,7 @@ class _WishlistScreenState extends State<WishlistScreen> {
         return Obx(
           () {
             return GridView.builder(
+              shrinkWrap: true,
               itemCount: wishlistController.wishlist.length,
               gridDelegate: delegate(si),
               itemBuilder: (context, index) {
