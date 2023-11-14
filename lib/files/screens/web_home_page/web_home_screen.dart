8bdc5b1a8c4a11b5bad1d37e7cbdbdd1c70de100
Shiv <sky.shivy@gmail.com>
2023-11-14 09:26:10 +0530

    new change

--- lib/files/screens/web_home_page/web_home_screen.dart
+++ lib/files/screens/web_home_page/web_home_screen.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/instance_manager.dart';
+
 import 'package:mtn_sa_revamp/enums/font_enum.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_buttons/custom_button.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_gredient.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_scroll_by_key.dart';
 import 'package:mtn_sa_revamp/files/custom_files/custom_text/custom_text.dart';
-import 'package:mtn_sa_revamp/files/custom_files/subscription_view.dart';
-import 'package:mtn_sa_revamp/files/localization/localizatio_service.dart';
-import 'package:mtn_sa_revamp/files/model/tune_info_model.dart';
-import 'package:mtn_sa_revamp/files/screens/my_tune_setting_screen/my_tune_settng_screen.dart';
+
 import 'package:mtn_sa_revamp/files/screens/navigation_bar/sub_view/home_search_widget/home_search_widget.dart';
-import 'package:mtn_sa_revamp/files/screens/popup_views/buy_popup_screen.dart/buy_opt_view.dart';
+
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_category/home_category_view.dart';
-import 'package:mtn_sa_revamp/files/screens/web_home_page/home_info_page/home_info_page.dart';
+
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_page_banner/home_page_banner.dart';
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_page_bottom_section/home_page_bottom_section.dart';
 import 'package:mtn_sa_revamp/files/screens/web_home_page/home_recomended/home_rec_view.dart';
-import 'package:mtn_sa_revamp/files/store_manager/store_manager.dart';
-import 'package:mtn_sa_revamp/files/utility/colors.dart';
+
 import 'package:mtn_sa_revamp/files/utility/string.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -36,11 +32,11 @@ class WebLandingPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CustomText(
-                  title: testLanguageTextStr.tr,
-                  fontName: FontName.aheavy,
-                  fontSize: 20,
-                ),
+                // CustomText(
+                //   title: testLanguageTextStr.tr,
+                //   fontName: FontName.aheavy,
+                //   fontSize: 20,
+                // ),
 
                 //homeSearchTopView(),
                 const LandingPageBanner(), //ProfileScreen(), //MyTuneScreen(), //
