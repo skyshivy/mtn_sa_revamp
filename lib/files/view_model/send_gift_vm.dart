766d581201a73cedc20028e5406d85c9d9eff032
Shiv <sky.shivy@gmail.com>
2023-11-14 15:12:38 +0530

    send gift

---
+++ lib/files/view_model/send_gift_vm.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:mtn_sa_revamp/files/service_call/service_call.dart';
+import 'package:mtn_sa_revamp/files/store_manager/store_manager.dart';
+import 'package:mtn_sa_revamp/files/utility/urls.dart';
+
+class SendGiftVM {
+  Future<Map<String, dynamic>?> send(
+      String bParty, String toneId, String packName, String toneName) async {
+    var parts = [];
+    // Random random = Random();
+    // int randomNumber = random.nextInt(1000000000);
+
+    Map<String, dynamic> body = {
+      'msisdnA': StoreManager().msisdn,
+      'msisdnB': bParty,
+      'toneId': toneId,
+      'channel': channelId,
+      'username': StoreManager().msisdn,
+      'language': StoreManager().languageCode,
+      'packName': packName, //'3',
+      'toneName': toneName,
+    };
+    body.forEach((key, value) {
+      parts.add('${Uri.encodeQueryComponent(key)}='
+          '${Uri.encodeQueryComponent(value.toString())}');
+    });
+    var formData = parts.join('&');
+    print("\nformed data is \n$formData\n");
+
+    Map<String, dynamic>? request =
+        await ServiceCall().post(sendGiftUrl, formData);
+    print("request is $request");
+    return request;
+  }
+}
